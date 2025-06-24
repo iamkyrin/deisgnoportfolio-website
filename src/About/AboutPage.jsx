3bf614c6dbf9e99eba1660a6de630d8c3a9492ce
Joshua Walters <151950421+TourniquetTears@users.noreply.github.com>
2025-06-24 15:04:53 +0200

    changes

--- src/About/AboutPage.jsx
+++ src/About/AboutPage.jsx
@@ -1,120 +1,138 @@
-
-import { Link } from "react-router-dom"
-
-function AboutPage(){
-return(
+import { Link } from "react-router-dom";
+import aboutHero from "/public/assets/about/mobile/image-about-hero.jpg";
+import aboutHeroDesk from "/public/assets/about/desktop/image-about-hero.jpg";
+import worldClass from "/public/assets/about/desktop/image-world-class-talent.jpg";
+import worldClassMobile from "/public/assets/about/mobile/image-world-class-talent.jpg";
+import icon1 from "/public/assets/location icons/1.svg";
+import icon2 from "/public/assets/location icons/2.svg";
+import icon3 from "/public/assets/location icons/3.svg";
+import realDealImg from "/public/assets/about/mobile/image-real-deal.jpg";
+import realDealImgDesk from "/public/assets/about/desktop/image-real-deal.jpg";
+function AboutPage() {
+  return (
     <>
-        <div className="flex justify-center items-center 2xl:flex-row">
-            <div className="flex justify-center items-center orange overflow-hidden lg:rounded-[20px] md:rounded-[20px] max-w-full 2xl:mt-20 md:mt-20 mt-15 2xl:w-[80rem] lg:w-[80rem] md:w-[45rem] w-full 2xl:h-[35rem] md:h-[20rem] lg:h-[35rem] max-h-screen">
-                
-                        <div className="2xl:ml-10 md:ml-10 lg:ml-20 flex flex-col 2xl:gap-10 lg:gap-10 md:gap-5 gap-10 2xl:mt-20 md:mt-5 lg:mt-20 mt-0 2xl:mr-20 lg:mr-20 md:mr-20">
-                            <img src = "src/assets/about/mobile/image-about-hero.jpg" className="max-w-full w-full sm:block md:hidden lg:hidden 2xl:hidden h-full" alt = 'hero section'></img>
-                            <h1 className="text-white 2xl:text-5xl lg:text-4xl md:text-lg text-2xl 2xl:text-left text-center lg:text-left md:text-left jost500 2xl:mt-0 lg:mt-0 md:mt-0 mt-0">About Us</h1>
-                            <p className="text-white 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-15">Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients’ audiences.
-                            </p>
-                        </div>
-                <div className="2xl:flex 2xl:justify-end md:flex md:justify-end lg:flex lg:justify-end">
-                    <img src = 'src/assets/about/desktop/image-about-hero.jpg' className="2xl:w-[200rem] md:w-[200rem] lg:w-[200rem] max-w-full hidden md:block lg:block 2xl:block"></img>
-                </div>
-            </div>
+      <div className="flex justify-center items-center 2xl:flex-row">
+        <div className="flex justify-center items-center orange overflow-hidden lg:rounded-[20px] md:rounded-[20px] max-w-full 2xl:mt-20 md:mt-20 mt-15 2xl:w-[80rem] lg:w-[80rem] md:w-[45rem] w-full 2xl:h-[35rem] md:h-[20rem] lg:h-[35rem] max-h-screen">
+          <div className="2xl:ml-10 md:ml-10 lg:ml-20 flex flex-col 2xl:gap-10 lg:gap-10 md:gap-5 gap-10 2xl:mt-20 md:mt-5 lg:mt-20 mt-0 2xl:mr-20 lg:mr-20 md:mr-20">
+            <img
+              src={aboutHero}
+              className="max-w-full w-full sm:block md:hidden lg:hidden 2xl:hidden h-full"
+              alt="hero section"
+            ></img>
+            <h1 className="text-white 2xl:text-5xl lg:text-4xl md:text-lg text-2xl 2xl:text-left text-center lg:text-left md:text-left jost500 2xl:mt-0 lg:mt-0 md:mt-0 mt-0">
+              About Us
+            </h1>
+            <p className="text-white 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-15">
+              Founded in 2010, we are a creative agency that produces lasting
+              results for our clients. We’ve partnered with many startups,
+              corporations, and nonprofits alike to craft designs that make real
+              impact. We’re always looking forward to creating brands, products,
+              and digital experiences that connect with our clients’ audiences.
+            </p>
+          </div>
+          <div className="2xl:flex 2xl:justify-end md:flex md:justify-end lg:flex lg:justify-end">
+            <img
+              src={aboutHeroDesk}
+              className="2xl:w-[200rem] md:w-[200rem] lg:w-[200rem] max-w-full hidden md:block lg:block 2xl:block"
+            ></img>
+          </div>
         </div>
+      </div>
 
-
-        <div className="flex justify-center items-center 2xl:flex-row">
-            <div className="flex justify-center items-center grayish overflow-hidden lg:rounded-[20px] md:rounded-[20px] max-w-full 2xl:mt-20 md:mt-20 mt-15 2xl:w-[80rem] lg:w-[80rem] md:w-[45rem] w-full 2xl:h-[35rem] md:h-[20rem] lg:h-[35rem] max-h-screen">
-                <div className="2xl:flex 2xl:justify-end md:flex md:justify-end lg:flex lg:justify-end">
-                    <img src = 'src/assets/about/desktop/image-world-class-talent.jpg' className="2xl:w-[100rem] md:w-[80rem] lg:w-[100rem] max-w-full hidden md:block lg:block 2xl:block object-cover"></img>
-                </div>
-                <div className="2xl:ml-10 md:ml-10 lg:ml-20 flex flex-col 2xl:gap-10 lg:gap-10 md:gap-5 gap-10 2xl:mt-20 md:mt-5 lg:mt-20 mt-0 2xl:mr-20 lg:mr-20 md:mr-0">
-                            <img src = "src/assets/about/mobile/image-world-class-talent.jpg" className="max-w-full w-full sm:block md:hidden lg:hidden 2xl:hidden h-full object-cover" alt = 'hero section'></img>
-                            <h1 className="2xl:text-5xl lg:text-4xl md:text-lg text-2xl 2xl:text-left text-center lg:text-left md:text-left jost500 2xl:mt-0 lg:mt-0 md:mt-0 mt-0 orangetext">World-class talent</h1>
-                            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-15">We are a crew of strategists, problem-solvers, and technologists. Every design is thoughtfully crafted from concept to launch, ensuring success in its given market. We are constantly updating our skills in a myriad of platforms.
-                            </p>
-                            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-30">Our team is multi-disciplinary and we are not merely interested in form — content and meaning are just as important. We give great importance to craftsmanship, service, and prompt delivery. Clients have always been impressed with our high-quality outcomes that encapsulates their brand’s story and mission.
-                            </p>
-                </div>
-
-            </div>
+      <div className="flex justify-center items-center 2xl:flex-row">
+        <div className="flex justify-center items-center grayish overflow-hidden lg:rounded-[20px] md:rounded-[20px] max-w-full 2xl:mt-20 md:mt-20 mt-15 2xl:w-[80rem] lg:w-[80rem] md:w-[45rem] w-full 2xl:h-[35rem] md:h-[20rem] lg:h-[35rem] max-h-screen">
+          <div className="2xl:flex 2xl:justify-end md:flex md:justify-end lg:flex lg:justify-end">
+            <img
+              src={worldClass}
+              className="2xl:w-[100rem] md:w-[80rem] lg:w-[100rem] max-w-full hidden md:block lg:block 2xl:block object-cover"
+            ></img>
+          </div>
+          <div className="2xl:ml-10 md:ml-10 lg:ml-20 flex flex-col 2xl:gap-10 lg:gap-10 md:gap-5 gap-10 2xl:mt-20 md:mt-5 lg:mt-20 mt-0 2xl:mr-20 lg:mr-20 md:mr-0">
+            <img
+              src={worldClassMobile}
+              className="max-w-full w-full sm:block md:hidden lg:hidden 2xl:hidden h-full object-cover"
+              alt="hero section"
+            ></img>
+            <h1 className="2xl:text-5xl lg:text-4xl md:text-lg text-2xl 2xl:text-left text-center lg:text-left md:text-left jost500 2xl:mt-0 lg:mt-0 md:mt-0 mt-0 orangetext">
+              World-class talent
+            </h1>
+            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-15">
+              We are a crew of strategists, problem-solvers, and technologists.
+              Every design is thoughtfully crafted from concept to launch,
+              ensuring success in its given market. We are constantly updating
+              our skills in a myriad of platforms.
+            </p>
+            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-30">
+              Our team is multi-disciplinary and we are not merely interested in
+              form — content and meaning are just as important. We give great
+              importance to craftsmanship, service, and prompt delivery. Clients
+              have always been impressed with our high-quality outcomes that
+              encapsulates their brand’s story and mission.
+            </p>
+          </div>
         </div>
+      </div>
 
-
-
-
-        <div className="flex justify-center mt-20 mb-20">
-            <div className="grid sm:grid-cols-3 grid-cols-1 gap-20">
-                <div className="flex flex-col justify-center items-center gap-10">
-                    <img src = 'src/assets/location icons/1.svg'></img>
-                    <h1 className="text-4xl md:text-2xl">CANADA
-</h1>
-                    <button className="py-4 px-8 orangeHover rounded-[10px] text-white text-xl cursor-pointer transition-colors duration-300">
-                        
-                        
-                        
-                        
-                        <Link to = {'/locations'}>
-                        
-                        See Location
-                        
-                        </Link>
-                        
-                        
-                        
-                        
-                        
-                        </button>
-                </div>
-                <div className="flex flex-col justify-center items-center gap-10">
-                    <img src = 'src/assets/location icons/2.svg'></img>
-                    <h1 className="text-4xl md:text-2xl">AUSTRALIA</h1>
-                    <button className="py-4 px-8 orangeHover rounded-[10px] text-white text-xl cursor-pointer transition-colors duration-300">
-                        
-
-                        <Link to = {'/locations'}>
-                        
-                        See Location
-                        
-                        </Link>
-
-
-
-                    </button>
-                </div>
-                <div className="flex flex-col justify-center items-center gap-10">
-                    <img src = 'src/assets/location icons/3.svg'></img>
-                    <h1 className="text-4xl md:text-2xl">UNITED KINGDOM</h1>
-                    <button className="py-4 px-8 orangeHover rounded-[10px] text-white text-xl cursor-pointer transition-colors duration-300">
-                        
-                        <Link to = {'/locations'}>
-                        
-                        See Location
-                        
-                        </Link>
-                        
-                        
-                        
-                        </button>
-                </div>
-            </div>
+      <div className="flex justify-center mt-20 mb-20">
+        <div className="grid sm:grid-cols-3 grid-cols-1 gap-20">
+          <div className="flex flex-col justify-center items-center gap-10">
+            <img src={icon1}></img>
+            <h1 className="text-4xl md:text-2xl">CANADA</h1>
+            <button className="py-4 px-8 orangeHover rounded-[10px] text-white text-xl cursor-pointer transition-colors duration-300">
+              <Link to={"/locations"}>See Location</Link>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-10">
+            <img src={icon2}></img>
+            <h1 className="text-4xl md:text-2xl">AUSTRALIA</h1>
+            <button className="py-4 px-8 orangeHover rounded-[10px] text-white text-xl cursor-pointer transition-colors duration-300">
+              <Link to={"/locations"}>See Location</Link>
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-10">
+            <img src={icon3}></img>
+            <h1 className="text-4xl md:text-2xl">UNITED KINGDOM</h1>
+            <button className="py-4 px-8 orangeHover rounded-[10px] text-white text-xl cursor-pointer transition-colors duration-300">
+              <Link to={"/locations"}>See Location</Link>
+            </button>
+          </div>
         </div>
+      </div>
 
-
-        <div className="flex justify-center items-center 2xl:flex-row">
-            <div className="flex justify-center items-center grayish overflow-hidden lg:rounded-[20px] md:rounded-[20px] max-w-full 2xl:mt-20 md:mt-20 mt-15 2xl:w-[80rem] lg:w-[80rem] md:w-[45rem] w-full 2xl:h-[35rem] md:h-[20rem] lg:h-[35rem] max-h-screen">
-                <div className="2xl:ml-10 md:ml-10 lg:ml-20 flex flex-col 2xl:gap-10 lg:gap-10 md:gap-5 gap-10 2xl:mt-20 md:mt-5 lg:mt-20 mt-0 2xl:mr-20 lg:mr-20 md:mr-10">
-                            <img src = "src/assets/about/mobile/image-real-deal.jpg" className="max-w-full w-full sm:block md:hidden lg:hidden 2xl:hidden h-full object-cover" alt = 'hero section'></img>
-                            <h1 className="2xl:text-5xl lg:text-4xl md:text-lg text-2xl 2xl:text-left text-center lg:text-left md:text-left jost500 2xl:mt-0 lg:mt-0 md:mt-0 mt-0 orangetext">The real deal</h1>
-                            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-15">As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.
-                            </p>
-                            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-30">We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.
-                            </p>
-                </div>
-                <div className="2xl:flex 2xl:justify-end md:flex md:justify-end lg:flex lg:justify-end">
-                    <img src = 'src/assets/about/desktop/image-real-deal.jpg' className="2xl:w-[100rem] md:w-[80rem] lg:w-[100rem] max-w-full hidden md:block lg:block 2xl:block object-cover"></img>
-                </div>
-            </div>
+      <div className="flex justify-center items-center 2xl:flex-row">
+        <div className="flex justify-center items-center grayish overflow-hidden lg:rounded-[20px] md:rounded-[20px] max-w-full 2xl:mt-20 md:mt-20 mt-15 2xl:w-[80rem] lg:w-[80rem] md:w-[45rem] w-full 2xl:h-[35rem] md:h-[20rem] lg:h-[35rem] max-h-screen">
+          <div className="2xl:ml-10 md:ml-10 lg:ml-20 flex flex-col 2xl:gap-10 lg:gap-10 md:gap-5 gap-10 2xl:mt-20 md:mt-5 lg:mt-20 mt-0 2xl:mr-20 lg:mr-20 md:mr-10">
+            <img
+              src={realDealImg}
+              className="max-w-full w-full sm:block md:hidden lg:hidden 2xl:hidden h-full object-cover"
+              alt="hero section"
+            ></img>
+            <h1 className="2xl:text-5xl lg:text-4xl md:text-lg text-2xl 2xl:text-left text-center lg:text-left md:text-left jost500 2xl:mt-0 lg:mt-0 md:mt-0 mt-0 orangetext">
+              The real deal
+            </h1>
+            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-15">
+              As strategic partners in our clients’ businesses, we are ready to
+              take on any challenge as our own. Solving real problems require
+              empathy and collaboration, and we strive to bring a fresh
+              perspective to every opportunity. We make design and technology
+              more accessible and give you tools to measure success.
+            </p>
+            <p className="text-black 2xl:text-xl lg:text-xl md:text-sm text-lg 2xl:text-left lg:text-left md:text-left text-center 2xl:mb-20 lg:mb-20 md:mb-0 mb-30">
+              We are visual storytellers in appealing and captivating ways. By
+              combining business and marketing strategies, we inspire audiences
+              to take action and drive real results.
+            </p>
+          </div>
+          <div className="2xl:flex 2xl:justify-end md:flex md:justify-end lg:flex lg:justify-end">
+            <img
+              src={realDealImgDesk}
+              className="2xl:w-[100rem] md:w-[80rem] lg:w-[100rem] max-w-full hidden md:block lg:block 2xl:block object-cover"
+            ></img>
+          </div>
         </div>
+      </div>
     </>
-)
+  );
 }
 
-export default AboutPage
+export default AboutPage;
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,73 +1,83 @@
-function Footer(){
-return(
-    <>
-        <div className="flex justify-center items-center h-20 mt-40 relative">
-            <div className="flex flex-col orange max-w-full w-[80rem] 2xl:rounded-[15px] lg:rounded-[15px] md:rounded-[15px] rounded-[0px]">
-                    <div className="flex flex-col gap-5 2xl:ml-10 lg:ml-10 md:ml-10 ml-2 max-w-full 2xl:w-[40rem] lg:w-[40rem] mt-15">
-                        <h1 className="text-white 2xl:text-5xl lg:text-5xl md:text-3xl md: jost700">Let's talk about your project</h1>
-                        <p className="text-white 2xl:text-xl lg:text-xl md:text-lg text-sm">Ready to take it to the next level? Contact us today and find out how our expertise can help your business grow.</p>
-                    </div>
-                    <div className="flex flex-row justify-end px-20">
-                        <button className="bg-white max-w-full w-50 rounded-[5px] text-center px-2 py-3 2xl:text-xl lg:text-xl md:text-xl text-lg mb-10 2xl:mt-0 lg:mt-0 md:mt-0 mt-5">GET IN TOUCH</button>
-                    </div>
-            </div>
-        </div>
+import logoLight from "/public/assets/logo/logo-light.png";
 
-    <section className="flex justify-center max-w-full items-center mt-10 bg-black py-15 text-white">
+function Footer() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-20 mt-40 relative">
+        <div className="flex flex-col orange max-w-full w-[80rem] 2xl:rounded-[15px] lg:rounded-[15px] md:rounded-[15px] rounded-[0px]">
+          <div className="flex flex-col gap-5 2xl:ml-10 lg:ml-10 md:ml-10 ml-2 max-w-full 2xl:w-[40rem] lg:w-[40rem] mt-15">
+            <h1 className="text-white 2xl:text-5xl lg:text-5xl md:text-3xl md: jost700">
+              Let's talk about your project
+            </h1>
+            <p className="text-white 2xl:text-xl lg:text-xl md:text-lg text-sm">
+              Ready to take it to the next level? Contact us today and find out
+              how our expertise can help your business grow.
+            </p>
+          </div>
+          <div className="flex flex-row justify-end px-20">
+            <button className="bg-white max-w-full w-50 rounded-[5px] text-center px-2 py-3 2xl:text-xl lg:text-xl md:text-xl text-lg mb-10 2xl:mt-0 lg:mt-0 md:mt-0 mt-5">
+              GET IN TOUCH
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <section className="flex justify-center max-w-full items-center mt-10 bg-black py-15 text-white">
         <div className="flex justify-between 2xl:px-85 lg:px-85 md:px-15 px-10 w-full max-w-full mt-20 text-white 2xl:flex-row lg:flex-row md:flex-row flex-col gap-5">
-            <div className="">
-                <img src="src/assets/logo/logo-light.png" alt="Business Logo" className="w-[15rem]"/>
-            </div>
+          <div className="">
+            <img src={logoLight} alt="Business Logo" className="w-[15rem]" />
+          </div>
           <nav className="jost400">
             <ul className="flex 2xl:flex-row lg:flex-row md:flex-row flex-col gap-10">
-              <li><a className="cursor-pointer">OUR COMPANY</a></li>
-              <li><a className="cursor-pointer">LOCATIONS</a></li>
-              <li><a className="cursor-pointer">CONTACT</a></li>
+              <li>
+                <a className="cursor-pointer">OUR COMPANY</a>
+              </li>
+              <li>
+                <a className="cursor-pointer">LOCATIONS</a>
+              </li>
+              <li>
+                <a className="cursor-pointer">CONTACT</a>
+              </li>
             </ul>
           </nav>
         </div>
-
       </section>
-<footer className="flex 2xl:flex-row lg:flex-row md:flex-row flex-col 2xl:gap-75 lg:gap-75 md:gap-10 gap-0 2xl:justify-center 2lg:items-center lg:justify-center md:items-center md:justify-center justify-left 2xl:px-0 lg:px-0 md:px-0 px-10 2xl:items-center bg-black text-white">
-    <div className="flex flex-col gap-2">
-                <h1 className='text-white/50 font-bold'>
-                    Designo Central Office
-                </h1>
-                <p className='text-white/50'>
-                    3886 Wellington Street
-                </p>
-                <p className='text-white/50'>
-                    Toronto, Ontario M9C 3J5
-                </p>
-    </div>
-    <div className="flex flex-col gap-2">
-                <h1 className='text-white/50 font-bold'>
-                    Contact Us Central Office
-                </h1>
-                <p className='text-white/50'>
-                  P : +1 253-863-8967
-                </p>
-                <p className='text-white/50'>
-                    M : contact@designo.co
-                </p>
-    </div>
-
-    <div className="flex flex-col gap-2">
-        <div>
-            <img></img>
-            <img></img>
-            <img></img>
-            <img></img>
-            <img></img>
+      <footer className="flex 2xl:flex-row lg:flex-row md:flex-row flex-col 2xl:gap-75 lg:gap-75 md:gap-10 gap-0 2xl:justify-center 2lg:items-center lg:justify-center md:items-center md:justify-center justify-left 2xl:px-0 lg:px-0 md:px-0 px-10 2xl:items-center bg-black text-white">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-white/50 font-bold">Designo Central Office</h1>
+          <p className="text-white/50">3886 Wellington Street</p>
+          <p className="text-white/50">Toronto, Ontario M9C 3J5</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-white/50 font-bold">Contact Us Central Office</h1>
+          <p className="text-white/50">P : +1 253-863-8967</p>
+          <p className="text-white/50">M : contact@designo.co</p>
         </div>
 
-        <p>© Easybank Template 2023. All Rights</p>
-        <p>Reserved Developed by <a target = '_blank' href = 'https://github.com/iamkyrin' className="transition text-red-500 hover:text-red-300 cursor-pointer">Joshua Walters</a></p>
-    </div>
-</footer>
-      
+        <div className="flex flex-col gap-2">
+          <div>
+            <img></img>
+            <img></img>
+            <img></img>
+            <img></img>
+            <img></img>
+          </div>
+
+          <p>© Easybank Template 2023. All Rights</p>
+          <p>
+            Reserved Developed by{" "}
+            <a
+              target="_blank"
+              href="https://github.com/iamkyrin"
+              className="transition text-red-500 hover:text-red-300 cursor-pointer"
+            >
+              Joshua Walters
+            </a>
+          </p>
+        </div>
+      </footer>
     </>
-)
+  );
 }
 
-export default Footer
+export default Footer;
